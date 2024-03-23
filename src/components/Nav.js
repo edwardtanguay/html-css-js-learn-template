e89f62114290cc2cf4fad8d829dfdf5e91db2e82
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-24 00:01:11 +0100

    menu works

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,9 +1,13 @@
+import { PageHome } from '../pages/PageHome.js';
+import { PageContact } from '../pages/PageContact.js';
+import { PageAbout } from '../pages/PageAbout.js';
+
 export const Nav = () => {
 
 	const onNavigate = (pathname) => {
 		window.history.pushState({}, pathname, window.location.origin + pathname);
 		// mainElem.innerHTML = routes[pathname];
-	}
+	};
 
 	setTimeout(() => {
 		const navElemLinkElems = document.querySelectorAll("nav a");
@@ -14,7 +18,19 @@ export const Nav = () => {
 				console.log('clicked');
 				// onNavigate(linkElem.getAttribute("href"));
 				const pageIdCode = linkElem.getAttribute("href");
-				pageOutputElem.innerHTML = 'page: ' + pageIdCode;
+				let html = '';
+				switch (pageIdCode) {
+					case '/':
+						html = PageHome();
+						break;
+					case '/contact':
+						html = PageContact();
+						break;
+					case '/about':
+						html = PageAbout();
+						break;
+				}
+				pageOutputElem.innerHTML = html;
 				event.preventDefault();
 			});
 		});
