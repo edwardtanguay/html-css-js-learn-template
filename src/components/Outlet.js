abe60b5dbbd6ec7c53580b37f74fe389c15ae464
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-25 07:58:29 +0100

    console.logs out

--- src/components/Outlet.js
+++ src/components/Outlet.js
@@ -1,12 +1,6 @@
 import { PageHome } from "../pages/PageHome.js";
 
 export const Outlet = () => {
-	const url = window.location.pathname;
-	console.log('url', url);
-
-	const url2 = window.location.href;
-	console.log('url2', url2);
-
 	return /*html*/ `
 		<section class="pageOutlet">${PageHome()}</section>
 	`;
