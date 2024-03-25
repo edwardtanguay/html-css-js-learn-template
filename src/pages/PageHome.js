bda674fd4630fcb6f48a39bc4f6b794473428821
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-25 14:23:28 +0100

    blockquote

--- src/pages/PageHome.js
+++ src/pages/PageHome.js
@@ -8,8 +8,9 @@ export const PageHome = () => {
 	<li>to give you a useful HTML/CSS/JS <b>starter site</b> with many <b>features</b></li>
 	<li>to make you aware of the <b>limitations</b> of HTML/CSS/JavaScript sites (sites you can start in the browser or with VSCode's Live Server extension)</li>
 	<li>to make the reasons clear why as a web developer you should instead learn <b>JavaScript frameworks</b>, e.g. React, Vue, Angular (sites which require Node and which you start with <code>npm run dev</code>)</li>
-	<li>to allow you to work in a HTML/CSS/JavaScript site that has a <b>similar structure</b> to JavaScript frameworks</li>
 </ul>
+
+<blockquote>Using this template is a good way to transition your skills from 1990s HTML pages to modern JavaScript frameworks.</blockquote>
 
 <h2>Features</h2>
 <ul>
