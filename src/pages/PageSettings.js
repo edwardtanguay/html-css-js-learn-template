28145e71e6d3c5dec2f62b1e33422cb7a411afa1
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-26 00:43:09 +0100

    notes on two pages

--- src/pages/PageSettings.js
+++ src/pages/PageSettings.js
@@ -1,9 +1,9 @@
-import { AdminPanel } from "../../dev/OLDCODE/AdminPanel.js";
+import { ConstructionSign } from "../components/ConstructionSign.js";
 
 export const PageSettings = () => {
 	return /*html*/ `
-	<p>This is the site page.</p>
+	<p class="intro">This this is site settings page which demonstrates how to allow the user to change the functionality of the site itself, e.g. setting dark/light mode, setting a mock number of seconds that the data-loading pages should take to load in order to see how the loading spinner looks, and creating a mock error on the data-loading pages to see how error handling works.
 
-${AdminPanel()}
+	${ConstructionSign()}
 	`;
 };
