36be542a0d9c0e6b990d159c59a7c9c14d55baf4
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-25 23:03:45 +0100

    search out

--- src/components/FullJobs.js
+++ src/components/FullJobs.js
@@ -3,28 +3,7 @@ import * as config from '../config.js';
 
 export const FullJobs = (fullJobs) => {
 
-	setTimeout(() => {
-		const mobileInputElem = document.querySelector('.mobileSearchArea .searchText');
-		const computerInputElem = document.querySelector('.computerSearchArea .searchText');
-
-		if (config.getResponsiveStatus() === 'mobile') {
-			mobileInputElem.focus();
-		} else {
-			computerInputElem.focus();
-		}
-	}, 0);
-
 	return /*html*/ `
-	<div class="searchArea computerSearchArea">
-		Search <input class="searchText" placeholder="skill" /> in ${fullJobs.length} Jobs
-	</div>
-	<div class="searchArea mobileSearchArea">
-		Search in ${fullJobs.length} Jobs:
-		<div>
-		<input class="searchText" placeholder="skill" /> 
-		</div>
-	</div>
-
 	<div class="fullJobs">
 	${fullJobs.map(fullJob => {
 		return /*html*/ `
