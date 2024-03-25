3f92370eae8bcbd4342283984ffdf51625ca8588
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-25 17:13:33 +0100

    loading works

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ export const Loading = () => {
 	return /*html*/ `
 <div class="loading">
 	<i class="fa fa-cog spinner" aria-hidden="true"></i>
-	<i>Loading...</i>
+	<i class="loadingText">Loading...</i>
 </div>
 	`;
 };
