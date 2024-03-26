fa1ddcad876b41cbd0e5148a3dbef5f0be800c09
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-26 08:23:41 +0100

    prepared for oop

--- src/pages/PageLocalStorage.js
+++ src/pages/PageLocalStorage.js
@@ -1,14 +1,12 @@
 import { ConstructionSign } from "../components/ConstructionSign.js";
 import { Loading } from "../components/Loading.js";
-import * as appData from '../appData.js';
 
 export const PageLocalStorage = () => {
 
 	setTimeout(async () => {
-		const contentElem = document.querySelector('.pageLocalData .content');
+		const contentElem = document.querySelector('.pageLocalStorage .content');
 
 		try {
-			// const employees = await appData.getEmployees();
 			contentElem.innerHTML = ConstructionSign();
 		}
 		catch (e) {
@@ -18,7 +16,7 @@ export const PageLocalStorage = () => {
 	}, 0);
 
 	return /*html*/ `
-<section class="page pageLocalData">
+<section class="page pageLocalStorage">
 	<p class="intro">This is a todo app with full CRUD (Create, Read, Update, Delete) functionality, including resorting, searching and reading/writing to localStorage. Since HTML/CSS/JavaScript sites don't have state variables, and since CRUD sites with complex interactivity require some way to manage state, this todo app is based on a OOP class caled TodoManager which contains the state of the app, and has methods to manipulate with CRUD commands and render this state as HTML.
 		
 	<div class="content">
