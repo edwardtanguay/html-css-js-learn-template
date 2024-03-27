a46b2ecacd0b9907041ae00b53ec0f1c06c259a0
Edward Tanguay (windows) <edward@tanguay.info>
2024-03-27 13:44:34 +0100

    settings base page

--- src/pages/PageSettings.js
+++ src/pages/PageSettings.js
@@ -1,9 +1,27 @@
 import { ConstructionSign } from "../components/ConstructionSign.js";
+import { Loading } from "../components/Loading.js";
 
 export const PageSettings = () => {
+
+	setTimeout(async () => {
+		const contentElem = document.querySelector('.pageSettings .content');
+
+		try {
+			contentElem.innerHTML = ConstructionSign();
+		}
+		catch (e) {
+			contentElem.innerHTML = '<div class="errorMessage">' + e.message + '</div>';
+		}
+
+	}, 0);
+
 	return /*html*/ `
+<section class="page pageSettings">
 	<p class="intro">This this is site settings page which demonstrates how to allow the user to change the functionality of the site itself, e.g. setting dark/light mode, setting a mock number of seconds that the data-loading pages should take to load in order to see how the loading spinner looks, and creating a mock error on the data-loading pages to see how error handling works.
 
-	${ConstructionSign()}
+	<div class="content">
+		${Loading()}
+	</div>
+</section>
 	`;
 };
